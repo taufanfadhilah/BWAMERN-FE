59bccc05fb812a63484a2533efb6af34e35684de
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-08-24 08:44:16 +0700

    colors utilize

---
+++ src/parts/IconText.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "elemets/Button";
+
+export default function IconText() {
+  return (
+    <Button className="brand-text-icon" href="" type="link">
+      Hom<span className="text-gray-900">ie</span>
+    </Button>
+  );
+}
