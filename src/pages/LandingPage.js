c41716400fae10353b693519b316077a168a8163
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-08-24 08:29:58 +0700

    create Button component

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Button from "elemets/Button";
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <Button />
+      </div>
+    );
+  }
+}
