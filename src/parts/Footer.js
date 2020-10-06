d0eca19ab9f3d797cc4b627a77744cf386c40ff3
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-10-06 13:15:44 +0700

    create footer

---
+++ src/parts/Footer.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+import Button from "elemets/Button";
+import IconText from "parts/IconText";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col-auto" style={{ width: 350 }}>
+            <IconText />
+            <p className="brand-tagline">
+              We kaboom your beauty holiday instantly and memorable.
+            </p>
+          </div>
+          <div className="col-auto mr-5">
+            <h6 className="mt-2">For Beginners</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button type="link" href="/register">
+                  New Account
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/properties">
+                  Start Booking a Room
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/use-payments">
+                  Use Payments
+                </Button>
+              </li>
+            </ul>
+          </div>
+          <div className="col-2 mr-5">
+            <h6 className="mt-2">Explore Us</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button type="link" href="/register">
+                  Our Careers
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/properties">
+                  Privacy
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/use-payments">
+                  Terms & Conditions
+                </Button>
+              </li>
+            </ul>
+          </div>
+          <div className="col-3">
+            <h6 className="mt-2">Connect Us</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button type="link" href="mailto:support@staycation.id">
+                  support@staycation.id
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="tel:+6522122081996">
+                  021 - 2208 - 1999
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <span>Staycation, Kemang, Jakarta</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col text-center copyrights mt-4">
+            Copyright 2019 . All rights reserved . Staycation
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
